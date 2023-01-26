1e0d120e39399caba9e627f98250576e4907d70d
fattahsamit <fattahsamit@gmail.com>
2023-01-26 23:48:50 +0600

    concatination

---
+++ Milestone_4/17/concatination.js
@@ -0,0 +1,9 @@
+var price1 = 31;
+var price2 = 10;
+var totalPrice = price1 + price2;
+console.log(totalPrice + price1);
+console.log("aeffaefaef" + totalPrice + price1); // Makes it a string so everything goes side by side
+var firstName = "Fattah";
+var lastName = "Samit";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
