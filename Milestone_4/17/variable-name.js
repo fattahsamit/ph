8d7594eb1f10ee67ed787d8f4cff70dea9f3d70d
fattahsamit <fattahsamit@gmail.com>
2023-01-26 22:15:46 +0600

    variable naming convention

---
+++ Milestone_4/17/variable-name.js
@@ -0,0 +1,30 @@
+var price = 29;
+
+// 1. Variable name cannot be any reserved  keywords
+var false = 29;
+var return = true;
+
+// 2. Variable name has to be in one word. No space
+var my home address = "New York";
+// How to use long names 
+// Cannot use dash
+var user-name = "Shahrukh Khan";
+var user_name = "Shahrukh Khan";
+// snake_case
+var my_home_address = "New York";
+// camelCase [We will use this one for js]
+var myHomeAddress = "New York";
+// PascalCase
+var MyHomeAddress = "New York";
+
+// 3. variable name cannot have quotation
+var "name" = "Tom Cruise";
+var name = "Tom Cruise";
+
+// 4. Variable name cannot start with a number but can end with a number
+var 19club = 1924;
+var club19 = 1924
+
+// Variable name is case sensitive
+var person = 25;
+var Person = 45;
