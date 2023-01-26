e01dd6889a28f0befdcf036fa278d292d767f8d6
fattahsamit <fattahsamit@gmail.com>
2023-01-27 00:09:04 +0600

    data type conversion

---
+++ Milestone_4/17/conversion.js
@@ -0,0 +1,22 @@
+var price1 = "31";
+var price2 = "10";
+var totalPrice = price1 + price2;
+console.log(totalPrice);
+
+price1 = "31";
+price2 = 10;
+totalPrice = price1 + price2;
+console.log(totalPrice);
+
+// Convert String to Number
+console.log(price1);
+price1 = parseInt(price1);
+console.log(price1);
+
+// Convert String to Float and integer
+var gpa = 3.75;
+console.log(gpa);
+gpa = parseFloat(gpa);
+console.log(gpa);
+gpa = parseInt(gpa);
+console.log(gpa);
