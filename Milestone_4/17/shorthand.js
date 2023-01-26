71d3a1790a483ba4a0d145aa76c4233b95539541
fattahsamit <fattahsamit@gmail.com>
2023-01-26 23:34:10 +0600

    shorthand

---
+++ Milestone_4/17/shorthand.js
@@ -0,0 +1,37 @@
+// Variable Declaration
+var tomatoPrice = 25;
+// Variable Value Update
+tomatoPrice = 30;
+
+// Add something to the previous value
+var price1 = 31;
+var price2 = 20;
+
+price1 = price1 + 10;
+console.log(price1);
+
+// addition to the same variable shorthand
+// += adding some value to the existing value
+price1 += 10;
+console.log(price1);
+
+// -=
+price2 = price2 - 5;
+console.log(price2);
+price2 -= 5;
+console.log(price2);
+
+// *=
+price2 *= 5;
+console.log(price2);
+
+// /=
+price2 /= 5;
+console.log(price2);
+
+// increment++ decrement--
+var age = 15;
+age = age + 1;
+age++;
+age--;
+console.log(age);
