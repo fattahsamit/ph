305fd0e42d7023e3aa62f443c0b2bb3a0fca3bee
fattahsamit <fattahsamit@gmail.com>
2023-01-27 00:52:36 +0600

    typeof operator usage for different data types

---
+++ Milestone_4/17/types.js
@@ -0,0 +1,12 @@
+// Check Data type
+var price = 100;
+console.log(typeof price);
+
+price = "100";
+console.log(typeof price);
+
+price = true;
+console.log(typeof price);
+
+var disco;
+console.log(typeof disco);
