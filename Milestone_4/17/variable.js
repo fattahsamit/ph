98a64fe4dd694b501ece34b2645b5d724e159ad5
fattahsamit <fattahsamit@gmail.com>
2023-01-26 21:13:04 +0600

    data types

--- Milestone_4/17/variable.js
+++ Milestone_4/17/variable.js
@@ -1,8 +1,21 @@
 // Variable
+
+// Numeric
 var price = 13;
 var age = 24;
 var temperature = 38;
 var money = 5;
 var weight = 65;
 var height = 5.5;
-console.log(height);
+
+// String
+var name = "Fattah Samit";
+var location = "Dhaka";
+var actress = "Natalie Portman";
+
+// Boolean
+var serious = true;
+var isFullMarks = false;
+var isSingle = true;
+
+console.log(isSingle);
