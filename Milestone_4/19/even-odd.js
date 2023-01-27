8c52d810398fd1913c65e4b50973f1d9a9bda252
fattahsamit <fattahsamit@gmail.com>
2023-01-28 00:21:39 +0600

    even odd using while loop

---
+++ Milestone_4/19/even-odd.js
@@ -0,0 +1,23 @@
+// Even numbers between 1 to 10
+var num = 2;
+while (num <= 10) {
+  //   if (num % 2 == 0) {
+  //     console.log(num);
+  //   }
+  //   num++;
+
+  console.log(num);
+  num = num + 2;
+}
+
+// Odd numbers between 1 to 10
+var num = 1;
+while (num <= 10) {
+  //   if (num % 2 == 0) {
+  //     console.log(num);
+  //   }
+  //   num++;
+
+  console.log(num);
+  num += 2;
+}
