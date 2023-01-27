8dde0f4b789973779ca396818fc48e9bdb8fba10
fattahsamit <fattahsamit@gmail.com>
2023-01-27 18:33:50 +0600

    multiple conditions with logical operators

---
+++ Milestone_4/18/multiple-conditions.js
@@ -0,0 +1,9 @@
+var isGraduated = true;
+var salary = 85000;
+var cars = 0;
+
+if (isGraduated === true && (salary > 50000 || cars >= 1)) {
+  console.log("Eligible");
+} else {
+  console.log("Not eligible");
+}
