ae47e562ee67dd4e7863ac37f42859356cd8ebe9
fattahsamit <fattahsamit@gmail.com>
2023-01-27 19:35:19 +0600

    practice problem 2

---
+++ Milestone_4/18/pp2.js
@@ -0,0 +1,65 @@
+var me = 85;
+var tom = 66;
+var jane = 95;
+var peter = 56;
+var john = 40;
+
+if (me >= 80) {
+  console.log("My Grade: A");
+} else if (me >= 60) {
+  console.log("My Grade: B");
+} else if (me >= 50) {
+  console.log("My Grade: C");
+} else if (me >= 40) {
+  console.log("My Grade: D");
+} else if (me <= 39) {
+  console.log("My Grade: F");
+}
+
+if (tom >= 80) {
+  console.log("Tom's Grade: A");
+} else if (tom >= 60) {
+  console.log("Tom's Grade: B");
+} else if (tom >= 50) {
+  console.log("Tom's Grade: C");
+} else if (tom >= 40) {
+  console.log("Tom's Grade: D");
+} else if (tom <= 39) {
+  console.log("TomMy Grade: 's Grade: F");
+}
+
+if (jane >= 80) {
+  console.log("Jane's Grade: A");
+} else if (jane >= 60) {
+  console.log("Jane's Grade: B");
+} else if (jane >= 50) {
+  console.log("Jane's Grade: C");
+} else if (jane >= 40) {
+  console.log("Jane's Grade: D");
+} else if (jane <= 39) {
+  console.log("Jane's Grade: F");
+}
+
+if (peter >= 80) {
+  console.log("Peter's Grade: A");
+} else if (peter >= 60) {
+  console.log("Peter's Grade: B");
+} else if (peter >= 50) {
+  console.log("Peter's Grade: C");
+} else if (peter >= 40) {
+  console.log("Peter's Grade: D");
+} else if (peter <= 39) {
+  console.log("Peter's Grade: F");
+}
+
+if (john >= 80) {
+  console.log("John's Grade: A");
+} else if (john >= 60) {
+  console.log("John's Grade: B");
+} else if (john >= 50) {
+  console.log("John's Grade: C");
+} else if (john >= 40) {
+  console.log("John's Grade: D");
+} else if (john <= 39) {
+  console.log("John's Grade: F");
+}
