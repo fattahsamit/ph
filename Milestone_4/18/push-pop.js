9e022cc5fb8299acfa9275c1a391e1dcc8221ec5
fattahsamit <fattahsamit@gmail.com>
2023-01-27 17:15:17 +0600

    push and pop methods in js

---
+++ Milestone_4/18/push-pop.js
@@ -0,0 +1,16 @@
+var numbers = [78, 45, 98, 85];
+// Use push to add element to an array as thye last element of the array
+numbers.push(69);
+console.log(numbers);
+
+var friends = ["Sam", "Eren", "Goku"];
+friends.push("Luffy");
+friends.push("Jake");
+console.log(friends);
+
+// Use pop to get rid of the last element
+numbers.pop();
+// The popped element stored in element variable
+var element = numbers.pop();
+console.log(numbers);
+console.log(element);
