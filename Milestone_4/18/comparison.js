8faf30820940077f0229d4ccc0dd416aca26bbd9
fattahsamit <fattahsamit@gmail.com>
2023-01-27 18:06:11 +0600

    comparison operator

---
+++ Milestone_4/18/comparison.js
@@ -0,0 +1,27 @@
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5 == 6);
+console.log(6 == 6);
+console.log(5 != 6);
+console.log(6 != 6);
+// less than or equal
+console.log(5 <= 6);
+console.log(6 <= 6);
+console.log(16 <= 6);
+// greater than or equal
+console.log(5 >= 6);
+console.log(5 >= 3);
+console.log(6 >= 6);
+
+var num1 = 69;
+var num2 = 88;
+console.log(num1 < num2);
+console.log(num1 > num2);
+console.log(num1 === num2);
+console.log(num1 !== num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+
+// Multiple conditions
+// money1 > money2 && result1 > result2 && height1 > height2
+// money1 > money2 || result1 > result2 || height1 > height2
