1657ba328e1d9592cc7280a2bda1dc457febff32
fattahsamit <fattahsamit@gmail.com>
2023-01-27 18:49:36 +0600

    Nested conditions

---
+++ Milestone_4/18/nested-conditions.js
@@ -0,0 +1,16 @@
+var math = true;
+var geometry = true;
+var trigonmetry = false;
+
+if (math == true) {
+  if (geometry == true) {
+    console.log("I'm good at geometry");
+    if (trigonmetry == true) {
+      console.log("also at trigonmetry");
+    } else {
+      console.log("but bad at trigonmetry");
+    }
+  }
+} else {
+  console.log("I'm really bad at math");
+}
