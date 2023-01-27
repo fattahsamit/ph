c01e82a081658acd8a4e9e101ff08d4b0505ca94
fattahsamit <fattahsamit@gmail.com>
2023-01-27 16:59:22 +0600

    array index

---
+++ Milestone_4/18/index.js
@@ -0,0 +1,13 @@
+var num = [44, 65, 84, 45, 97, 75];
+
+// Get element value by index
+console.log(num[1]);
+
+// Set element value by index
+num[1] = 77;
+num[2] = 33;
+console.log(num);
+
+// Find index of an element
+var positionIndex = num.indexOf(97);
+console.log(positionIndex);
