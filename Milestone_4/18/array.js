c2fcccb94802f10726b46f8804c6bd777c64585f
fattahsamit <fattahsamit@gmail.com>
2023-01-27 16:49:29 +0600

    js array

---
+++ Milestone_4/18/array.js
@@ -0,0 +1,6 @@
+var friendsAge = [11, 21, 45, 17, 14, 105, 6];
+var picnicFee = [5000, 2000, 4000, 150];
+var actress = ["maya", "chaya", "toya", "niki"];
+var vowels = ["a", "e", "i", "o", "u"];
+
+console.log(friendsAge.length);
