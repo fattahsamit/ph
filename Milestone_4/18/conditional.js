1cf0e4bbcd13a28499b519396aa7574a06ebc36d
fattahsamit <fattahsamit@gmail.com>
2023-01-27 18:23:06 +0600

    conditional statements

---
+++ Milestone_4/18/conditional.js
@@ -0,0 +1,16 @@
+var price = 87000;
+var budget = 94000;
+
+if (budget > price) {
+  console.log("I can buy the product");
+} else {
+  console.log("I cannot buy the product");
+}
+
+var chickenPrice = 500;
+var foodBudget = 500;
+if (chickenPrice <= foodBudget) {
+  console.log("I can eat chicken");
+} else {
+  console.log("I can eat rice");
+}
