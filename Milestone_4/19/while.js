2e8538a5ebdcfe66f5d701b74c71311cc104467b
fattahsamit <fattahsamit@gmail.com>
2023-01-27 23:32:11 +0600

    while loop

---
+++ Milestone_4/19/while.js
@@ -0,0 +1,8 @@
+var roastGiven = 0;
+while (roastGiven < 7) {
+  console.log("Give me my roast!");
+  //   roastGiven = roastGiven + 1;
+  //   roastGiven += 1;
+  console.log(roastGiven);
+  roastGiven++;
+}
