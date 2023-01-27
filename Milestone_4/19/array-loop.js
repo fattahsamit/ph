ff350be81bb8ff495b6c012cfc1d63954fd18107
fattahsamit <fattahsamit@gmail.com>
2023-01-28 02:09:36 +0600

    loop using an array

---
+++ Milestone_4/19/array-loop.js
@@ -0,0 +1,13 @@
+// for (var i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+// Display every elements of an array
+var numbers = [45, 87, 89, 32, 52, 23, 144];
+for (var i = 0; i < numbers.length; i++) {
+  console.log(numbers[i]);
+}
+
+var items = ["ant", "mouse", "cat", "dog", "elephant"];
+for (var i = 0; i < items.length; i++) {
+  console.log(items[i]);
+}
