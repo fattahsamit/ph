73279ffd7545ff6dc621cd3cf58b426aa9a62a95
fattahsamit <fattahsamit@gmail.com>
2023-01-31 20:36:29 +0600

    js object

---
+++ Milestone_4/20/object.js
@@ -0,0 +1,25 @@
+var student = {
+  id: 153,
+  name: "John Doe",
+  class: 10,
+  marks: 96,
+};
+console.log(student);
+
+var mobile = {
+  brand: "Nokia",
+  price: 5000,
+  storage: "64 GB",
+  camera: "12 MP",
+};
+console.log(mobile.brand);
+
+var myComputer = {
+  brand: "HP",
+  price: 70000,
+  color: "Silver",
+  processor: "i5",
+};
+console.log(myComputer.price);
+myComputer.price = 50000;
+console.log(myComputer);
