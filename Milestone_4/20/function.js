70bfb8a5e3e4a64435a3cdbcd76feafa2aeb6624
fattahsamit <fattahsamit@gmail.com>
2023-01-31 16:36:26 +0600

    function parameter

--- Milestone_4/20/function.js
+++ Milestone_4/20/function.js
@@ -1,7 +1,20 @@
 // Function Declaration
 function functionName(money) {
   console.log("HELLO WORLD!!!");
-  console.log(money);
+  console.log("I have", money, "Taka");
 }
 // Function Call
-functionName(200);
+let taka = 200;
+functionName(taka);
+
+function add(num1, num2) {
+  console.log(num1, "+", num2, "=", num1 + num2);
+}
+add(2, 3);
+
+function sum(a, b, c, d, e) {
+  console.log(a, b, c, d, e);
+  let sum = a + b + c + d + e;
+  console.log(sum);
+}
+sum(34, 52, 62, 89, 3);
