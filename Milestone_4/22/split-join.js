0843f2c23306ef66929f3a423433cdeca9412b20
fattahsamit <fattahsamit@gmail.com>
2023-02-02 14:38:21 +0600

    split, slice, substring, join

---
+++ Milestone_4/22/split-join.js
@@ -0,0 +1,19 @@
+const lyrics =
+  "I got a long way to go And a long memory. Ive been searching for an answer Always just out of reach. Blood on the floor Sirens repeat. Ive been searching for the courage To face my enemies";
+// const parts = lyrics.split("");
+// const parts = lyrics.split(".");
+const parts = lyrics.split(" ");
+// console.log(parts);
+
+const partial = lyrics.slice(8, 12);
+// console.log(partial);
+
+const partial2 = lyrics.substring(8, 12);
+// console.log(partial2);
+
+const lines = [
+  "sgvsdgsdg sdgsdrg sdfgwagikhmn rghe",
+  "ikfgmfgnfg dfhdbe ge egger ggedge wfegvbgd",
+  "bdfbdf fwefw iingb fbxbx",
+];
+console.log(lines.join(", "));
