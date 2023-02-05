5a599f1ce39f8c26a9691848ea923e3afca8f1ce
fattahsamit <fattahsamit@gmail.com>
2023-02-05 18:48:51 +0600

    calculate total cost with quantity

--- Milestone_4/23/simpleCart.js
+++ Milestone_4/23/simpleCart.js
@@ -1,15 +1,15 @@
 const shoppingCart = [
-  { name: "shoe", phone: 1200 },
-  { name: "shirt", phone: 2200 },
-  { name: "pant", phone: 3700 },
-  { name: "belt", phone: 600 },
+  { name: "shoe", price: 1200 },
+  { name: "shirt", price: 2200 },
+  { name: "pant", price: 3700 },
+  { name: "belt", price: 600 },
 ];
 
 function totalCost(products) {
   let sum = 0;
   for (let i = 0; i < products.length; i++) {
     const product = products[i];
-    sum += product.phone;
+    sum += product.price;
   }
   return sum;
 }
