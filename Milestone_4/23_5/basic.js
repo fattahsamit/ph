14e6be665b82852643b0a6cac057088acec53e9a
fattahsamit <fattahsamit@gmail.com>
2023-02-06 13:36:10 +0600

    Alternative way and comments

--- Milestone_4/23_5/basic.js
+++ Milestone_4/23_5/basic.js
@@ -1,3 +1,4 @@
+// 1+2+3+...+n
 function add(n) {
   let sum = 0;
   for (let i = 1; i <= n; i++) {
@@ -9,18 +10,20 @@ function add(n) {
 const sum = add(9);
 console.log(sum);
 
+// Table of 3
 function tableOf3(n) {
   let table = [];
-  for (let i = 1; i <= n; i++) {
-    const mul = 3 * i;
+  for (let i = 1; i <= 10; i++) {
+    const mul = n * i;
     table.push(mul);
   }
   return table;
 }
 
-const table3 = tableOf3(10);
+const table3 = tableOf3(3);
 console.log(table3);
 
+// Show numbers larger than 5
 function largerThan5(numbers) {
   let count = 0;
   for (let i = 0; i < numbers.length; i++) {
@@ -36,6 +39,7 @@ const numbers = [-1, 2, -3, 4, 5, 6, -7, 8, -9, 10];
 const bigger = largerThan5(numbers);
 console.log(bigger);
 
+// Find the largest name and show it in reverse
 function largerReverse(name1, name2) {
   let longerName;
   if (name1.length > name2.length) {
@@ -43,6 +47,10 @@ function largerReverse(name1, name2) {
   } else {
     longerName = name2;
   }
+  // let reversedName = "";
+  // for (let i = longerName.length - 1; i >= 0; i--) {
+  //   reversedName = reversedName + longerName[i];
+  // }
   const reversedName = longerName.split("").reverse().join("");
   return reversedName;
 }
@@ -52,6 +60,7 @@ const name1 = "Mofiz",
 const reverse = largerReverse(name1, name2);
 console.log(reverse);
 
+// Convert Milliliter to Liter
 function milliliterToLiter(ml) {
   const l = ml / 1000;
   return l;
@@ -61,6 +70,7 @@ const ml = 3500;
 const liter = milliliterToLiter(ml);
 console.log(liter);
 
+// Find the youngest person from the array of objects containing person's data
 function smallestPerson(persons) {
   let smallestAge = persons[0].age;
   let smallest = persons[0].name;
