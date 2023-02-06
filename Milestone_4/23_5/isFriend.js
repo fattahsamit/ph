c2a509b9f79ad1383d21d6da44245a956cb2d66d
fattahsamit <fattahsamit@gmail.com>
2023-02-06 14:16:03 +0600

    check if both are friends or not

---
+++ Milestone_4/23_5/isFriend.js
@@ -0,0 +1,12 @@
+function isBestFriend(friend1, friend2) {
+  if (friend1.name == friend2.friend && friend2.name == friend1.friend) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const friend1 = { name: "doe", friend: "alex" };
+const friend2 = { name: "john", friend: "doe" };
+const isFriend = isBestFriend(friend1, friend2);
+console.log(isFriend);
