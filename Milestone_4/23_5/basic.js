8ba365384d53809979b5d195d1df6e4542289c90
fattahsamit <fattahsamit@gmail.com>
2023-02-06 12:34:55 +0600

    find the smallest aged person name

--- Milestone_4/23_5/basic.js
+++ Milestone_4/23_5/basic.js
@@ -59,4 +59,27 @@ function milliliterToLiter(ml) {
 
 const ml = 3500;
 const liter = milliliterToLiter(ml);
-console.log(liter);
+// console.log(liter);
+
+function smallestPerson(persons) {
+  let smallestAge = persons[0].age;
+  let smallest = persons[0].name;
+  for (let i = 0; i < persons.length; i++) {
+    const personAge = persons[i].age;
+    const smallestPerson = persons[i].name;
+    if (personAge < smallestAge) {
+      smallestAge = personAge;
+      smallest = smallestPerson;
+    }
+  }
+  return smallest;
+}
+
+const persons = [
+  { name: "sakib", age: 30 },
+  { name: "samiul", age: 20 },
+  { name: "sahid", age: 50 },
+  { name: "samin", age: 10 },
+];
+const smallest = smallestPerson(persons);
+console.log(smallest);
