d525eab92d71d07b0c1fb6762dc9c054c728f696
fattahsamit <fattahsamit@gmail.com>
2023-02-11 01:05:31 +0600

    queryselector

--- Milestone_5/25/js/first.js
+++ Milestone_5/25/js/first.js
@@ -8,8 +8,21 @@ console.log(allTitles);
 
 const secondSection = document.getElementById("second-section");
 console.log(secondSection);
+// secondSection.getAttribute("id");
+secondSection.setAttribute("title", "Tooltip set by JavaScript");
 secondSection.style.color = "white";
+secondSection.style.textAlign = "center";
 secondSection.style.backgroundColor = "steelblue";
 
 // document.querySelectorAll (NodeList)
 // document.querySelector (selects first match)
+
+// querySelector
+const firstLi = document.querySelector("#third-section-list li");
+console.log(firstLi.innerText);
+// querySelectorAll
+const someLi = document.querySelectorAll(".third-section li");
+console.log(someLi);
+for (const li of someLi) {
+  console.log(li.innerText);
+}
