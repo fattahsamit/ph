3a9fa4c42fe8dd187576afc73d36b2ba855f70c6
fattahsamit <fattahsamit@gmail.com>
2023-02-10 23:51:26 +0600

    innerHTML

---
+++ Milestone_5/25/js/third.js
@@ -0,0 +1,15 @@
+console.log("third");
+
+const main = document.getElementById("main-container");
+const section = document.createElement("section");
+section.innerHTML = `
+    <h1> My Dynamic Section </h1>
+    <p> Extra text added inside paragraph </p>
+    <ul> 
+        <li> first item </li>
+        <li> second item </li>
+        <li> third item </li>
+        <li> fourth item </li>
+    </ul> 
+`;
+main.appendChild(section);
