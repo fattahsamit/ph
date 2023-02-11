77f1f06140dfe81ed11d104757ab9f3eeb588ec3
fattahsamit <fattahsamit@gmail.com>
2023-02-11 19:42:37 +0600

    addEventListener

--- Milestone_5/26/js/events.js
+++ Milestone_5/26/js/events.js
@@ -1,8 +1,14 @@
 console.log("Separate JS file");
 
 // Option 1 Directly set on the HTML element
+{
+  /* <button onclick="document.body.style.backgroundColor='yellow'"> */
+}
 
-// Option 2 Add onClick Function
+// Option 2 Add onClick Function on the HTML element
+{
+  /* <button onclick="makeRed()">Make Red</button> */
+}
 function makeRed() {
   document.body.style.backgroundColor = "red";
 }
@@ -18,3 +24,19 @@ const makeGreenButton = document.getElementById("make-green");
 makeGreenButton.onclick = function makeGreen() {
   document.body.style.backgroundColor = "green";
 };
+
+// Option 4
+const purpleButton = document.getElementById("make-purple");
+purpleButton.addEventListener("click", makePurple);
+function makePurple() {
+  document.body.style.backgroundColor = "purple";
+}
+
+const pinkButton = document.getElementById("make-pink");
+pinkButton.addEventListener("click", function makePink() {
+  document.body.style.backgroundColor = "pink";
+});
+
+document.getElementById("make-orange").addEventListener("click", function () {
+  document.body.style.backgroundColor = "orange";
+});
