0cadca3ede0c1c935d50a982e8f0a8ca770542f4
fattahsamit <fattahsamit@gmail.com>
2023-02-11 18:41:47 +0600

    onclick handler

--- Milestone_5/26/js/events.js
+++ Milestone_5/26/js/events.js
@@ -1,1 +1,20 @@
 console.log("Separate JS file");
+
+// Option 1 Directly set on the HTML element
+
+// Option 2 Add onClick Function
+function makeRed() {
+  document.body.style.backgroundColor = "red";
+}
+
+// Option 3
+const makeBlueButton = document.getElementById("make-blue");
+makeBlueButton.onclick = makeBlue;
+function makeBlue() {
+  document.body.style.backgroundColor = "blue";
+}
+
+const makeGreenButton = document.getElementById("make-green");
+makeGreenButton.onclick = function makeGreen() {
+  document.body.style.backgroundColor = "green";
+};
