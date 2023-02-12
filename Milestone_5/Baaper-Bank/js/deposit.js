c183133eac6bd9418dacc04d0a893c3c49e9dbcd
fattahsamit <fattahsamit@gmail.com>
2023-02-12 16:55:59 +0600

    Update deposit and balance and handle string add

--- Milestone_5/Baaper-Bank/js/deposit.js
+++ Milestone_5/Baaper-Bank/js/deposit.js
@@ -3,12 +3,32 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
   //   Step 2: Get the deposit amount from the deposit input field
   //   For input field use .value to get the value inside the input field
   const depositField = document.getElementById("deposit-field");
-  const depositAmount = depositField.value;
+  const newDepositAmountString = depositField.value;
+  const newDepositAmount = parseFloat(newDepositAmountString);
+  //   console.log(typeof newDepositAmount);
 
   //   Step 3: Get the current deposit total amount
   //   For Non-input(element other than input, textarea) use innerText to get the text
   const depositTotalElement = document.getElementById("deposit-total");
-  let depositTotal = depositTotalElement.innerText;
-  depositTotalElement.innerText = depositAmount;
-  console.log(depositTotal);
+  const previousDepositTotalString = depositTotalElement.innerText;
+  //   Convert String to Floating point number
+  const previousDepositTotal = parseFloat(previousDepositTotalString);
+
+  //   Step 4: add numbers to set the total deposit
+  const currentDepositTotal = previousDepositTotal + newDepositAmount;
+  //   Set the deposit total
+  depositTotalElement.innerText = currentDepositTotal;
+
+  // Step 5: Get Balance Current Total
+  const balanceTotalElement = document.getElementById("balance-total");
+  const previousBalanceTotalString = balanceTotalElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+  //   Step 6: Calculate Current Total Balance
+  const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+  //   Set the balance total
+  balanceTotalElement.innerText = currentBalanceTotal;
+
+  //   Step 7: Clear the deposit field
+  depositField.value = "";
 });
