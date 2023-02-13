2cce58ce1390be1cf59c7d0d02805bcbdb1ba708
fattahsamit <fattahsamit@gmail.com>
2023-02-13 18:00:20 +0600

    shared utility to use functions globally

--- Milestone_5/Functional-Baaper-Bank/js/deposit.js
+++ Milestone_5/Functional-Baaper-Bank/js/deposit.js
@@ -1,23 +1,3 @@
-function getInputFieldValueById(inputFieldId) {
-  const inputField = document.getElementById(inputFieldId);
-  const inputFieldValueString = inputField.value;
-  const inputFieldValue = parseFloat(inputFieldValueString);
-  inputField.value = "";
-  return inputFieldValue;
-}
-
-function getTextElementValueById(elementId) {
-  const textElement = document.getElementById(elementId);
-  const textElementValueString = textElement.innerText;
-  const textElementValue = parseFloat(textElementValueString);
-  return textElementValue;
-}
-
-function setTextElementValueById(elementId, newValue) {
-  const textElement = document.getElementById(elementId);
-  textElement.innerText = newValue;
-}
-
 document.getElementById("btn-deposit").addEventListener("click", function () {
   /*
   1. Get the element by ID
