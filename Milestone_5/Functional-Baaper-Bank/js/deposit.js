74c146ecfe302a58e29283eb1096a4628b78cc79
fattahsamit <fattahsamit@gmail.com>
2023-02-13 17:56:13 +0600

    Get and set element value using functions

--- Milestone_5/Functional-Baaper-Bank/js/deposit.js
+++ Milestone_5/Functional-Baaper-Bank/js/deposit.js
@@ -6,11 +6,16 @@ function getInputFieldValueById(inputFieldId) {
   return inputFieldValue;
 }
 
-function getElementValueById(elementId) {
-  const element = document.getElementById(elementId);
-  const elementValueString = element.innerText;
-  const elementValue = parseFloat(elementValueString);
-  return elementValue;
+function getTextElementValueById(elementId) {
+  const textElement = document.getElementById(elementId);
+  const textElementValueString = textElement.innerText;
+  const textElementValue = parseFloat(textElementValueString);
+  return textElementValue;
+}
+
+function setTextElementValueById(elementId, newValue) {
+  const textElement = document.getElementById(elementId);
+  textElement.innerText = newValue;
 }
 
 document.getElementById("btn-deposit").addEventListener("click", function () {
@@ -20,7 +25,18 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
   3. Convert string value to a number
   */
   const newDepositAmount = getInputFieldValueById("deposit-field");
-  const previousDepositTotal = getElementValueById("deposit-total");
-  console.log(newDepositAmount);
-  console.log(previousDepositTotal);
+
+  //   Get previous deposit total by ID
+  const previousDepositTotal = getTextElementValueById("deposit-total");
+
+  //   Calculate new deposit total
+  const newDepositTotal = previousDepositTotal + newDepositAmount;
+
+  //   Set deposit total value
+  setTextElementValueById("deposit-total", newDepositTotal);
+
+  //   Get previous balance total by ID
+  const previousBalanceTotal = getTextElementValueById("balance-total");
+  const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+  setTextElementValueById("balance-total", newBalanceTotal);
 });
