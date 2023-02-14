3105fcc0f7ba4e62a71e40850406d3f327b23f8f
fattahsamit <fattahsamit@gmail.com>
2023-02-14 18:20:27 +0600

    coupon confirmation function and eventlistener

--- Milestone_5/discount-calculator/js/calculator.js
+++ Milestone_5/discount-calculator/js/calculator.js
@@ -1,1 +1,17 @@
 console.log("hello");
+
+function coupon30Confirmation(coupon) {
+  document.getElementById(coupon).addEventListener("keyup", function (event) {
+    const line = event.target.value;
+    if (line === "DISC30") {
+      return true;
+    } else {
+      return false;
+    }
+  });
+}
+
+document.getElementById("btn-apply").addEventListener("click", function () {
+  if (coupon30Confirmation("coupon-input")) {
+  }
+});
