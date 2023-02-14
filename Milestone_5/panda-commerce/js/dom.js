a7322d89d9215a252e133997076f9844a491880f
fattahsamit <fattahsamit@gmail.com>
2023-02-14 12:32:56 +0600

    make h2 elements lightblue

--- Milestone_5/panda-commerce/js/dom.js
+++ Milestone_5/panda-commerce/js/dom.js
@@ -1,1 +1,6 @@
 console.log("Hello World");
+
+const headerElements = document.getElementsByTagName("h3");
+for (const headerElement of headerElements) {
+  headerElement.style.color = "lightblue";
+}
