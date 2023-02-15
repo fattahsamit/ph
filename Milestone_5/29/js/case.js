74bb1be675de4f3b8959391ae84f48d1dcf99b9d
fattahsamit <fattahsamit@gmail.com>
2023-02-15 23:43:59 +0600

    update Total Price of Case

--- Milestone_5/29/js/case.js
+++ Milestone_5/29/js/case.js
@@ -21,14 +21,25 @@ function updateCaseNumber(IsIncrease) {
   }
 
   caseNumberField.value = newCaseNumber;
+  return newCaseNumber;
+}
+
+function updateCaseTotalPrice(newCaseNumber) {
+  const caseTotalPrice = newCaseNumber * 59;
+  const caseTotalElement = document.getElementById("case-total");
+  caseTotalElement.innerText = caseTotalPrice;
 }
 
 document.getElementById("btn-case-plus").addEventListener("click", function () {
-  updateCaseNumber(true);
+  const newCaseNumber = updateCaseNumber(true);
+
+  updateCaseTotalPrice(newCaseNumber);
 });
 
 document
   .getElementById("btn-case-minus")
   .addEventListener("click", function () {
-    updateCaseNumber(false);
+    const newCaseNumber = updateCaseNumber(false);
+
+    updateCaseTotalPrice(newCaseNumber);
   });
