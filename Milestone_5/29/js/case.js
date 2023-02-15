c0cc786ef09ad1eaaf8b2f7f60783e738d250d93
fattahsamit <fattahsamit@gmail.com>
2023-02-15 23:29:20 +0600

    shopping cart handle decrease, using function

--- Milestone_5/29/js/case.js
+++ Milestone_5/29/js/case.js
@@ -1,9 +1,34 @@
-document.getElementById("btn-case-plus").addEventListener("click", function () {
+/*
+  1. Add event listener to the case plus button
+  2. Get the value inside the case number field (input field)
+  3. Convert the number to an integer
+  4. Calculate the new case number
+  5. Set the value to the case number field
+*/
+
+function updateCaseNumber(IsIncrease) {
   const caseNumberField = document.getElementById("case-number-field");
   const caseNumberString = caseNumberField.value;
-  const caseNumber = parseInt(caseNumberString);
+  const previousCaseNumber = parseInt(caseNumberString);
 
-  const newCaseNumber = caseNumber + 1;
+  let newCaseNumber;
+
+  // if (IsIncrease === true) {
+  if (IsIncrease) {
+    newCaseNumber = previousCaseNumber + 1;
+  } else {
+    newCaseNumber = previousCaseNumber - 1;
+  }
 
   caseNumberField.value = newCaseNumber;
+}
+
+document.getElementById("btn-case-plus").addEventListener("click", function () {
+  updateCaseNumber(true);
 });
+
+document
+  .getElementById("btn-case-minus")
+  .addEventListener("click", function () {
+    updateCaseNumber(false);
+  });
