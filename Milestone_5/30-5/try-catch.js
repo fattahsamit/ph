f0dd90ba4b5e4e0cbadebf12324820231a8dcf78
fattahsamit <fattahsamit@gmail.com>
2023-02-19 17:47:08 +0600

    try catch finally

---
+++ Milestone_5/30-5/try-catch.js
@@ -0,0 +1,19 @@
+console.log("Hello");
+console.log(25);
+// console.log(yo);
+// let yo = "yo";
+console.log(200);
+console.log(100);
+
+try {
+  console.log("inside try");
+  //   console.log(student);
+  console.log(yo);
+  let yo = "yo";
+  console.log("after error");
+} catch (error) {
+  console.log("inside catch");
+  console.log(error);
+} finally {
+  console.log("finally done");
+}
