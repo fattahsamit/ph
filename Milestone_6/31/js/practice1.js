4c8e0dd5a6e52efd21e0740eba06d58f453083ca
fattahsamit <fattahsamit@gmail.com>
2023-02-21 19:15:07 +0600

    practice problem 1

---
+++ Milestone_6/31/js/practice1.js
@@ -0,0 +1,10 @@
+const problemOne = (one, two, three) => one + two + three;
+console.log(problemOne(2, 3, 4));
+
+const problemTwo = `I am a web developer. 
+I love to code. 
+I love to eat biryani.`;
+console.log(problemTwo);
+
+const problemThree = (one, two = 0) => one + two;
+console.log(problemThree(4));
