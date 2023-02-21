3088b289429da1db1d4a7b8a41176a73d981ec16
fattahsamit <fattahsamit@gmail.com>
2023-02-21 17:09:33 +0600

    Spread operator

---
+++ Milestone_6/31/js/spread.js
@@ -0,0 +1,19 @@
+const max = Math.max(12, 85, 999, 78);
+// console.log(max);
+const numbers = [12, 85, 999, 78];
+const largest = Math.max(...numbers);
+console.log(numbers);
+// Spread operator ...
+console.log(...numbers);
+console.log(largest);
+
+const numbers2 = [...numbers];
+numbers.push(55);
+numbers2.push(7777);
+console.log(numbers);
+console.log(numbers2);
+
+const numbers3 = [...numbers];
+
+const numbers4 = [48, 222, ...numbers, 111];
+console.log(numbers4);
