86537008193a38d16f8054c574a83afb0bdea7da
fattahsamit <fattahsamit@gmail.com>
2023-02-22 16:48:31 +0600

    map string array

---
+++ Milestone_6/32/more-map.js
@@ -0,0 +1,11 @@
+const numbers = [12, 56, 87, 44];
+const half = numbers.map((n) => n / 2);
+const third = numbers.map((n) => n / 3);
+// console.log(half);
+// console.log(third);
+
+const friends = ["Tom Cruise", "Tom Hanks", "Thomas Shelby", "Tommy Montana"];
+const firstLetters = friends.map((friends) => friends[0]);
+// console.log(firstLetters);
+const nameLength = friends.map((friends) => friends.length);
+console.log(nameLength);
