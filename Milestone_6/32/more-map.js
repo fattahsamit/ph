d8bf86f632255a2d3675522f05fe2dcca978fe9c
fattahsamit <fattahsamit@gmail.com>
2023-02-22 16:54:34 +0600

    array of objects map

--- Milestone_6/32/more-map.js
+++ Milestone_6/32/more-map.js
@@ -8,4 +8,17 @@ const friends = ["Tom Cruise", "Tom Hanks", "Thomas Shelby", "Tommy Montana"];
 const firstLetters = friends.map((friends) => friends[0]);
 // console.log(firstLetters);
 const nameLength = friends.map((friends) => friends.length);
-console.log(nameLength);
+// console.log(nameLength);
+
+const products = [
+  { id: 1, name: "laptop", price: 65000 },
+  { id: 2, name: "mobile", price: 90000 },
+  { id: 3, name: "watch", price: 5000 },
+  { id: 4, name: "tablet", price: 23000 },
+];
+
+const items = products.map((product) => product.name);
+const prices = products.map((p) => p.price);
+
+console.log(items);
+console.log(prices);
