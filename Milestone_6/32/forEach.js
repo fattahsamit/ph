eee5ee46af029d2b7ab9063763241e9459bda406
fattahsamit <fattahsamit@gmail.com>
2023-02-22 17:08:55 +0600

    forEach

---
+++ Milestone_6/32/forEach.js
@@ -0,0 +1,14 @@
+const friends = ["Tom Cruise", "Tom Hanks", "Thomas Shelby", "Tommy Montana"];
+
+friends.forEach((friend) => console.log(friend));
+
+const products = [
+  { id: 1, name: "laptop", price: 65000 },
+  { id: 2, name: "mobile", price: 90000 },
+  { id: 3, name: "watch", price: 5000 },
+  { id: 4, name: "tablet", price: 23000 },
+];
+
+products.forEach((product) => {
+  console.log(product.name);
+});
