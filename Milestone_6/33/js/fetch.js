27a921671ffd208443183e581679fe398eba028d
fattahsamit <fattahsamit@gmail.com>
2023-02-24 00:13:23 +0600

    JSON placeholder, GET data, display data on UI

---
+++ Milestone_6/33/js/fetch.js
@@ -0,0 +1,11 @@
+// https://jsonplaceholder.typicode.com/
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+const loadData = () => {
+  const url = "https://jsonplaceholder.typicode.com/todos/1";
+  fetch(url)
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+};
