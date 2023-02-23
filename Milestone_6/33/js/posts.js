9f42773fd6b0c2ad2bad7820d518ca273958ea02
fattahsamit <fattahsamit@gmail.com>
2023-02-24 01:51:23 +0600

    Load posts and display on the website with CSS

---
+++ Milestone_6/33/js/posts.js
@@ -0,0 +1,22 @@
+function loadPosts() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => displayPosts(data));
+}
+
+function displayPosts(posts) {
+  const postsContainer = document.getElementById("posts-container");
+  for (const post of posts) {
+    console.log(post);
+    const postArticle = document.createElement("article");
+    postArticle.classList.add("post");
+    postArticle.innerHTML = `
+    <h4>User-${post.userId}</h4>
+    <h5>Post: ${post.title}</h5>
+    <p>Post Description: ${post.body}</p>
+    `;
+    postsContainer.appendChild(postArticle);
+  }
+}
+
+loadPosts();
