1cd0c0e5e603f62df4417aebbde01487cd25297d
fattahsamit <fattahsamit@gmail.com>
2023-02-25 01:22:43 +0600

    async await

--- Milestone_6/34/js/quotes.js
+++ Milestone_6/34/js/quotes.js
@@ -1,7 +1,11 @@
-const loadQuote = () => {
-  fetch("https://api.kanye.rest/")
-    .then((res) => res.json())
-    .then((data) => displayQuote(data));
+const loadQuote = async () => {
+  try {
+    const res = await fetch("https://api.kanye.rest/");
+    const data = await res.json();
+    displayQuote(data);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const displayQuote = (quote) => {
