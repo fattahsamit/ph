bed501407787b325f344eb5d456a2d2ab6029c64
fattahsamit <fattahsamit@gmail.com>
2023-02-25 00:20:29 +0600

    display dynamic search result with bs cards

--- Milestone_6/34/js/meal.js
+++ Milestone_6/34/js/meal.js
@@ -1,11 +1,13 @@
-const loadMeals = () => {
-  fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=fish")
+const loadMeals = (searchText) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+  fetch(url)
     .then((res) => res.json())
     .then((data) => displayMeals(data.meals));
 };
 
 const displayMeals = (meals) => {
   const mealsContainer = document.getElementById("meals-container");
+  mealsContainer.innerText = "";
   // Container element
   meals.forEach((meal) => {
     console.log(meal);
@@ -15,14 +17,15 @@ const displayMeals = (meals) => {
     // Set content of the child element
     mealDiv.innerHTML = `
     <div class="card h-100">
-        <img src="..." class="card-img-top" alt="..." />
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
+            <h5 class="card-title">${meal.strMeal}</h5>
             <p class="card-text">
                 This is a longer card with supporting text below as a natural
                 lead-in to additional content. This content is a little bit
                 longer.
             </p>
+            <a href="${meal.strYoutube}">Recipe</a>
         </div>
     </div>
     `;
@@ -31,4 +34,11 @@ const displayMeals = (meals) => {
   });
 };
 
-loadMeals();
+const searchMeals = () => {
+  const searchText = document.getElementById("search-field").value;
+  //   Search Meals
+  console.log(searchText);
+  loadMeals(searchText);
+};
+
+loadMeals("chicken");
