146b86facba1ad99b67a7a59f3c97a1da2c7f033
fattahsamit <fattahsamit@gmail.com>
2023-02-25 18:48:56 +0600

    show selected player details

--- Milestone_6/Explore API/sports_db/sports.js
+++ Milestone_6/Explore API/sports_db/sports.js
@@ -4,7 +4,8 @@ const searchAllData = () => {
   const URL = `https://www.thesportsdb.com/api/v1/json/3/searchplayers.php?p=${inputValue}`;
   fetch(URL)
     .then((res) => res.json())
-    .then((data) => showPlayerData(data.player));
+    .then((data) => showPlayerData(data.player))
+    .catch((error) => console.log(error));
 };
 
 const showPlayerData = (players) => {
@@ -12,8 +13,8 @@ const showPlayerData = (players) => {
   const container = document.getElementById("player-info");
   container.innerHTML = "";
   players.forEach((player) => {
-    console.log(player);
-    const { strThumb, strPlayer, strNationality } = player;
+    // console.log(player);
+    const { strThumb, strPlayer, strNationality, idPlayer } = player;
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
@@ -25,7 +26,7 @@ const showPlayerData = (players) => {
             <h5 class="card-title">${strPlayer}</h5>
             <p class="card-text">Nationality: ${strNationality}</p>
             <div>
-            <button type="button" class="btn btn-primary">Details</button>
+            <button onclick="singlePlayer('${idPlayer}')" type="button" class="btn btn-primary">Details</button>
             <button type="button" class="btn btn-danger">Delete</button>
             </div>
         </div>
@@ -33,6 +34,40 @@ const showPlayerData = (players) => {
       `;
     container.appendChild(div);
   });
+};
+
+const singlePlayer = (id) => {
+  const URL = `https://www.thesportsdb.com/api/v1/json/3/lookupplayer.php?id=${id}
+    `;
+  //   console.log(URL);
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => showSinglePlayer(data.players[0]))
+    .catch((error) => console.log(error));
+};
+
+const showSinglePlayer = (data) => {
+  const { strThumb, strPlayer, strDescriptionEN } = data;
+  const container = document.getElementById("single-player-details");
+  const div = document.createElement("div");
+  div.innerHTML = `
+    <div class="card mb-3" w-100 h-100;">
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img src="${strThumb}" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${strPlayer}</h5>
+                    <p class="card-text">${
+                      strDescriptionEN.slice(0, 180) + "..."
+                    }</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  `;
+  container.appendChild(div);
 };
 
 // searchAllData();
