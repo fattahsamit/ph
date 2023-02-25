560dca759986e1c099e9ca824b89e23636c80d05
fattahsamit <fattahsamit@gmail.com>
2023-02-25 17:27:44 +0600

    search data from api

--- Milestone_6/Explore API/sports_db/sports.js
+++ Milestone_6/Explore API/sports_db/sports.js
@@ -1,1 +1,10 @@
-console.log("sports");
+const searchAllData = () => {
+  const inputElement = document.getElementById("search-value");
+  const inputValue = inputElement.value;
+  const URL = `https://www.thesportsdb.com/api/v1/json/3/searchplayers.php?p=${inputValue}`;
+  fetch(URL)
+    .then((res) => res.json())
+    .then((data) => console.log(data.player));
+};
+
+// searchAllData();
