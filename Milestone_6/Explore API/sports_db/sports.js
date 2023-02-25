42450b46e38e8c17ad0a652e583685cc8c433981
fattahsamit <fattahsamit@gmail.com>
2023-02-25 17:51:05 +0600

    object destructuring

--- Milestone_6/Explore API/sports_db/sports.js
+++ Milestone_6/Explore API/sports_db/sports.js
@@ -11,14 +11,15 @@ const showPlayerData = (players) => {
   const container = document.getElementById("player-info");
 
   players.forEach((player) => {
-    console.log(player);
+    // console.log(player);
+    const { strThumb, strPlayer } = player;
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
       <div class="card">
-          <img src="${player.strThumb}" class="card-img-top" alt="${player.strPlayer}">
+          <img src="${strThumb}" class="card-img-top" alt="${strPlayer}">
           <div class="card-body">
-            <h5 class="card-title">${player.strPlayer}</h5>
+            <h5 class="card-title">${strPlayer}</h5>
             <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
           </div>
         </div>
