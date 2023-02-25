e62f5855865179cfa6f5b8a35c0cdc96aa10a93d
fattahsamit <fattahsamit@gmail.com>
2023-02-25 14:17:20 +0600

    display data

--- Milestone_6/Explore API/json_placeholder/placeholder.js
+++ Milestone_6/Explore API/json_placeholder/placeholder.js
@@ -2,8 +2,21 @@ const loadData = () => {
   const url = "https://jsonplaceholder.typicode.com/posts";
   fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => showData(data))
     .catch((error) => console.log(error));
+};
+
+const showData = (data) => {
+  //   console.log(data.slice(0, 5));
+  const container = document.getElementById("post-info");
+  for (const singleData of data) {
+    console.log(singleData.title);
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <h1 class="text-2xl text-center">${singleData.title}</h1>
+    `;
+    container.appendChild(div);
+  }
 };
 
 loadData();
