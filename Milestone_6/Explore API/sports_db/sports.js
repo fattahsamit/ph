46cd29969049bc6129b31456cd3e1a6403c885fd
fattahsamit <fattahsamit@gmail.com>
2023-02-25 17:48:42 +0600

    display searched players

--- Milestone_6/Explore API/sports_db/sports.js
+++ Milestone_6/Explore API/sports_db/sports.js
@@ -4,7 +4,27 @@ const searchAllData = () => {
   const URL = `https://www.thesportsdb.com/api/v1/json/3/searchplayers.php?p=${inputValue}`;
   fetch(URL)
     .then((res) => res.json())
-    .then((data) => console.log(data.player));
+    .then((data) => showPlayerData(data.player));
+};
+
+const showPlayerData = (players) => {
+  const container = document.getElementById("player-info");
+
+  players.forEach((player) => {
+    console.log(player);
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `
+      <div class="card">
+          <img src="${player.strThumb}" class="card-img-top" alt="${player.strPlayer}">
+          <div class="card-body">
+            <h5 class="card-title">${player.strPlayer}</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+      `;
+    container.appendChild(div);
+  });
 };
 
 // searchAllData();
