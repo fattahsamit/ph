a51ac2fca52f43ed87c216fbef8eebf5c78d576d
fattahsamit <fattahsamit@gmail.com>
2023-02-25 14:35:24 +0600

    show function declaration

--- Milestone_6/Explore API/meme_gallery/gallery.js
+++ Milestone_6/Explore API/meme_gallery/gallery.js
@@ -5,8 +5,12 @@ const loadMeme = () => {
   const URL = "https://meme-api.com/gimme/20";
   fetch(URL)
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => showMeme(data.memes))
     .catch((error) => console.log(error));
+};
+
+const showMeme = (memes) => {
+  console.log(memes);
 };
 
 loadMeme();
