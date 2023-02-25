0e5d721d3bed4b4e8bf6502b2ffb9a1e8d9a874b
fattahsamit <fattahsamit@gmail.com>
2023-02-25 18:11:29 +0600

    ternary operator

--- Milestone_6/Explore API/sports_db/sports.js
+++ Milestone_6/Explore API/sports_db/sports.js
@@ -9,20 +9,26 @@ const searchAllData = () => {
 
 const showPlayerData = (players) => {
   const container = document.getElementById("player-info");
-
+  container.innerHTML = "";
   players.forEach((player) => {
-    // console.log(player);
-    const { strThumb, strPlayer } = player;
+    console.log(player);
+    const { strThumb, strPlayer, strNationality } = player;
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
-      <div class="card">
-          <img src="${strThumb}" class="card-img-top" alt="${strPlayer}">
-          <div class="card-body">
+    <div class="card">
+        <img src="${
+          strThumb ? strThumb : "https://picsum.photos/500/300?random=3"
+        }" class="card-img-top" alt="${strPlayer}">
+        <div class="card-body">
             <h5 class="card-title">${strPlayer}</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          </div>
+            <p class="card-text">Nationality: ${strNationality}</p>
+            <div>
+            <button type="button" class="btn btn-danger">Info</button>
+                <button type="button" class="btn btn-primary">Warning</button>
+            </div>
         </div>
+    </div>
       `;
     container.appendChild(div);
   });
