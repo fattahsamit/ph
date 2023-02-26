9e9a923048c0cd765e51ecec3cb0536844b93895
fattahsamit <fattahsamit@gmail.com>
2023-02-26 21:11:38 +0600

    display data from api

--- Milestone_6/35/js/app.js
+++ Milestone_6/35/js/app.js
@@ -2,26 +2,31 @@ const loadPhones = async (searchText) => {
   const URL = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   const res = await fetch(URL);
   const data = await res.json();
-  displayPhones(data);
+  displayPhones(data.data);
 };
 
 const displayPhones = (phones) => {
   console.log(phones);
   const phoneContainer = document.getElementById("phone-container");
-  const div = document.createElement("div");
-  div.classList.add("col");
-  div.innerHTML = `
-    <div class="card">
-        <img src="..." class="card-img-top" alt="..." />
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">
-                This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-            </p>
-        </div>
-    </div>
-  `;
-  phoneContainer.appendChild(div);
+
+  phones.forEach((phone) => {
+    console.log(phone);
+    const { phone_name, image } = phone;
+    const div = document.createElement("div");
+    div.classList.add("col");
+    div.innerHTML = `
+      <div class="card">
+          <img src="${image}" class="card-img-top" alt="..." />
+          <div class="card-body">
+              <h5 class="card-title">${phone_name}</h5>
+              <p class="card-text">
+                  This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+              </p>
+          </div>
+      </div>
+    `;
+    phoneContainer.appendChild(div);
+  });
 };
 
 loadPhones("iphone");
