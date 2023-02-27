0d009f7b3f8000ba003d347621d324395e05ff10
fattahsamit <fattahsamit@gmail.com>
2023-02-27 16:51:12 +0600

    Show All button to display all data

--- Milestone_6/35/js/app.js
+++ Milestone_6/35/js/app.js
@@ -1,16 +1,22 @@
-const loadPhones = async (searchText) => {
+const loadPhones = async (searchText, dataLimit) => {
   const URL = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
   const res = await fetch(URL);
   const data = await res.json();
-  displayPhones(data.data);
+  displayPhones(data.data, dataLimit);
 };
 
-const displayPhones = (phones) => {
+const displayPhones = (phones, dataLimit) => {
   console.log(phones);
   const phoneContainer = document.getElementById("phone-container");
   phoneContainer.textContent = "";
-  // Display 6 phones only
-  phones = phones.slice(0, 6);
+  // Display 10 phones only
+  const showAll = document.getElementById("show-all");
+  if (dataLimit && phones.length > 10) {
+    phones = phones.slice(0, 10);
+    showAll.classList.remove("d-none");
+  } else {
+    showAll.classList.add("d-none");
+  }
 
   // Display no phones found
   const noPhone = document.getElementById("no-found-message");
@@ -42,13 +48,18 @@ const displayPhones = (phones) => {
   toggleSpinner(false);
 };
 
-// Search button event
-document.getElementById("btn-search").addEventListener("click", () => {
+const processSearch = (dataLimit) => {
   toggleSpinner(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
-  loadPhones(searchText);
-  searchField.value = "";
+  loadPhones(searchText, dataLimit);
+};
+
+// Search button event
+document.getElementById("btn-search").addEventListener("click", () => {
+  // Start loader
+  processSearch(10);
+  // searchField.value = "";
 });
 
 const toggleSpinner = (isLoading) => {
@@ -59,3 +70,8 @@ const toggleSpinner = (isLoading) => {
     loaderSection.classList.add("d-none");
   }
 };
+
+// Not the best way to load show all
+document.getElementById("btn-show-all").addEventListener("click", function () {
+  processSearch();
+});
