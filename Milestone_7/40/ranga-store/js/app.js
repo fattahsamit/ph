8ce472e3a0c232e6420d8c2a5792f08aac5802ce
fattahsamit <fattahsamit@gmail.com>
2023-03-09 04:09:31 +0600

    Fix Cart, Price rounding, product quantity

--- Milestone_7/40/ranga-store/js/app.js
+++ Milestone_7/40/ranga-store/js/app.js
@@ -43,7 +43,7 @@ let count = 0;
 
 const addToCart = (id, price) => {
   count = count + 1;
-  updatePrice("price", value);
+  updatePrice("price", price);
 
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
@@ -73,9 +73,9 @@ const getInputValue = (id) => {
 // main price update function
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
-  const convertPrice = parseInt(value);
+  const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
