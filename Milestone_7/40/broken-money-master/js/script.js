58aa25113ddf446482176ae61b17ec6259f3c022
fattahsamit <fattahsamit@gmail.com>
2023-03-08 18:09:17 +0600

    debugging fix id and value error

--- Milestone_7/40/broken-money-master/js/script.js
+++ Milestone_7/40/broken-money-master/js/script.js
@@ -1,8 +1,8 @@
 const calculateExpense = () => {
-  const income = document.querySelector("income").value;
-  const food = document.querySelector("food").value;
-  const rent = document.querySelector("rent").value;
-  const clothes = document.querySelectorAll("clothes").value;
+  const income = document.querySelector("#income").value;
+  const food = document.querySelector("#food").value;
+  const rent = document.querySelector("#rent").value;
+  const clothes = document.querySelectorAll("#clothes")[0].value;
 
   if (
     income < 0 ||
@@ -21,9 +21,9 @@ const calculateExpense = () => {
   const expense = parseInt(food) + parseInt(rent) + parseInt(clothes);
 
   // calculate balance
-  const balance = parseInt(income.value) - expense;
+  const balance = parseInt(income) - expense;
   //   validate income
-  if (expense > income.value) {
+  if (expense > income) {
     alert("Expenses cannot be more than income");
   } else {
     // view total expense and balance
