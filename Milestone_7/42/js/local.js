d683104e1899cc605271f9d880d2f5972672b2ea
fattahsamit <fattahsamit@gmail.com>
2023-03-10 00:45:44 +0600

    add product and quantity

--- Milestone_7/42/js/local.js
+++ Milestone_7/42/js/local.js
@@ -1,5 +1,5 @@
 const getAge = window.localStorage.getItem("age");
 console.log(getAge);
 const setAge = () => {
-  const age = localStorage.setItem("age", 24);
+  localStorage.setItem("age", 24);
 };
