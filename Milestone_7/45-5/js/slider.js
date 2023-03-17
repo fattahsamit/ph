5eb5adbdb61f3c26b53e53f77b55fd5df9e1f363
fattahsamit <fattahsamit@gmail.com>
2023-03-17 16:09:55 +0600

    simple slider functionalities

--- Milestone_7/45-5/js/slider.js
+++ Milestone_7/45-5/js/slider.js
@@ -1,6 +1,22 @@
-let imageIndex = 0;
-
+const images = [
+  "images/pic-1.jpg",
+  "images/pic-2.jpg",
+  "images/pic-3.jpg",
+  "images/pic-4.jpg",
+  "images/pic-5.jpg",
+  "images/pic-6.jpg",
+  "images/pic-7.jpg",
+  "images/pic-8.jpg",
+  "images/pic-9.jpg",
+];
+let imageIndex = 1;
+const imgEl = document.getElementById("slider-img");
 setInterval(() => {
-  console.log(imageIndex);
+  if (imageIndex === images.length) {
+    imageIndex = 0;
+  }
+  //   console.log(imageIndex, images[imageIndex]);
+  const imgUrl = images[imageIndex];
+  imgEl.setAttribute("src", imgUrl);
   imageIndex++;
 }, 1000);
