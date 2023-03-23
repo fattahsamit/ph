f14c7e20ec1053405a5003f0735826d66a3921e0
fattahsamit <fattahsamit@gmail.com>
2023-03-23 16:17:44 +0600

    display rest countries

--- Milestone_8/46/restCountries/js/countries.js
+++ Milestone_8/46/restCountries/js/countries.js
@@ -6,7 +6,21 @@ const loadCountries = async () => {
 };
 
 const displayCountries = (countries) => {
-  console.log(countries[0]);
+  //   console.log(countries[0]);
+  const countriesHTML = countries.map((country) => getCountryHTML(country));
+  const container = document.getElementById("countries");
+  container.innerHTML = countriesHTML.join(" ");
+};
+
+const getCountryHTML = ({ name, flags, area, region }) => {
+  return `
+    <div class="country">
+        <h2>${name.common}</h2>
+        <p>Area: ${area}</p>
+        <p>Continent: ${region}</p>
+        <img src="${flags.png}">
+    </div>
+    `;
 };
 
 loadCountries();
