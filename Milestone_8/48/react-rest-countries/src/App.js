6e710e87493209d26f8da90e892727b0e15bb601
fattahsamit <fattahsamit@gmail.com>
2023-03-25 02:27:29 +0600

    load countries using useEffect

--- Milestone_8/48/react-rest-countries/src/App.js
+++ Milestone_8/48/react-rest-countries/src/App.js
@@ -1,8 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <LoadCountries></LoadCountries>
+    </div>
+  );
+}
+
+function LoadCountries() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const URL = "https://restcountries.com/v3.1/all";
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>World Tour</h1>
+      <h3>Available Countries: {countries.length}</h3>
+    </div>
+  );
 }
 
 export default App;
