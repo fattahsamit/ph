a25f316f70e28246db65061c63573395d14a69ef
fattahsamit <fattahsamit@gmail.com>
2023-03-24 16:22:31 +0600

    Multiple components from an array of objects

--- Milestone_8/47/my-app/src/App.js
+++ Milestone_8/47/my-app/src/App.js
@@ -1,23 +1,33 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-const name = 5534;
-const singer = { name: "Chester Bennington", band: "Linkin Park" };
-const singer2 = { name: "Michael Jackson", job: "Singer" };
-
-const singerStyle = {
-  color: "darkblue",
-  backgroundColor: "#61dafb",
-  padding: "10px",
-  borderRadius: "10px",
-};
+const singers = [
+  { name: "Chester Bennington", band: "Linkin Park" },
+  { name: "Matthew Healy", band: "1975" },
+  { name: "Adam Levine", band: "Maroon V" },
+  { name: "Michael Jackson", band: "Solo" },
+];
 
 function App() {
+  const names = [
+    "Fattah Samit",
+    "Sakib Al Hasan",
+    "Chris Evans",
+    "Saul Goodman",
+  ];
   return (
     <div className="App">
-      <Person name="Fattah Samit" job="Software Engineer"></Person>
-      <Person name="Sakib Al Hasan" job="Businessman"></Person>
-      <Person name="Chris Evans" job="Actor"></Person>
+      {names.map((name) => (
+        <Person name={name}></Person>
+      ))}
+
+      {singers.map((singer) => (
+        <Person name={singer.name}></Person>
+      ))}
+
+      {/* <Person name={names[0]} job="Software Engineer"></Person>
+      <Person name={names[1]} job="Businessman"></Person>
+      <Person name={names[2]} job="Actor"></Person> */}
       <h2 id="justAnotherComponent">New Component</h2>
       <Friend name="Jason Mamoa" role="Aquaman"></Friend>
       <Friend name="Jhonny Depp" role="Jack Sparrow"></Friend>
