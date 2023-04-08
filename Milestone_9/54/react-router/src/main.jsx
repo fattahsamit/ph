5a533f4db6f3a3de80692ef2e2f1909c3192e417
fattahsamit <fattahsamit@gmail.com>
2023-04-08 23:08:17 +0600

    react router setup

---
+++ Milestone_9/54/react-router/src/main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path: "/about",
+    element: <div>This is the about page</div>,
+  },
+  {
+    path: "/contact",
+    element: <div>Contact us now</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
