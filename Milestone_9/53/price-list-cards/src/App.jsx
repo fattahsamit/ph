dd75b17ddee2ae18f8dd9758667e69bbb9c8be84
fattahsamit <fattahsamit@gmail.com>
2023-04-08 16:24:16 +0600

    simple pricing options

--- Milestone_9/53/price-list-cards/src/App.jsx
+++ Milestone_9/53/price-list-cards/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import PriceList from "./components/PriceList/PriceList";
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
       <h1 className="text-7xl text-purple-500">Hello World</h1>
+      <PriceList></PriceList>
     </div>
   );
 }
---
+++ Milestone_9/53/price-list-cards/src/components/PriceCard/PriceCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const PriceCard = ({ price }) => {
+  return (
+    <div>
+      <h2>
+        <span className="text-5xl font-extrabold text-purple-700">
+          {price.price}
+        </span>
+        <span className="text-2xl">/month</span>
+      </h2>
+      <h5 className="text-2xl font-bold">{price.name}</h5>
+    </div>
+  );
+};
+
+export default PriceCard;
