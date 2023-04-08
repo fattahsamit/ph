1e41d03154131b9cc8562d05f1004f77e5efd3ad
fattahsamit <fattahsamit@gmail.com>
2023-04-08 17:49:35 +0600

    axios, data extraction, data processing bar chart

--- Milestone_9/53/price-list-cards/src/App.jsx
+++ Milestone_9/53/price-list-cards/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Dashboard from "./components/Dashboard/Dashboard";
 import Navbar from "./components/Navbar/Navbar";
+import PhoneBar from "./components/PhoneBar/PhoneBar";
 import PriceList from "./components/PriceList/PriceList";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <h1 className="text-7xl text-purple-500">Hello World</h1>
       <PriceList></PriceList>
       <Dashboard></Dashboard>
+      <PhoneBar></PhoneBar>
     </div>
   );
 }
