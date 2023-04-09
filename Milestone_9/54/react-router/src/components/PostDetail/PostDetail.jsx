7bbf40fd43a2286ccd7034d669f17e1659489c07
fattahsamit <fattahsamit@gmail.com>
2023-04-09 16:33:20 +0600

    useNavigate hook to go back

--- Milestone_9/54/react-router/src/components/PostDetail/PostDetail.jsx
+++ Milestone_9/54/react-router/src/components/PostDetail/PostDetail.jsx
@@ -1,16 +1,22 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 const PostDetail = () => {
   const post = useLoaderData();
   const { id, useId, title, body } = post;
   //   console.log(post);
 
+  const navigate = useNavigate();
+  const handleGoBack = () => {
+    navigate(-1);
+  };
+
   return (
     <div>
       <h3>{id}</h3>
       <h3>Title: {title}</h3>
       <p>{body}</p>
+      <button onClick={handleGoBack}>Go back</button>
     </div>
   );
 };
