96f1c75d749c539a228218aee35b2a784247466c
fattahsamit <fattahsamit@gmail.com>
2023-04-10 23:34:12 +0600

    display tshirt data and added clickHandler

--- Milestone_9/56/banglar-tshirt/src/components/Home/Home.jsx
+++ Milestone_9/56/banglar-tshirt/src/components/Home/Home.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import TShirt from "../TShirt/TShirt";
+import Cart from "../Cart/Cart";
+import "./Home.css";
 
 const Home = () => {
   const tshirts = useLoaderData();
 
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = (tshirt) => {
+    console.log(tshirt);
+  };
+
   return (
-    <div>
-      <h3>Number of T-shirts: {tshirts.length}</h3>
+    <div className="home-container">
+      <div className="t-shirts-container">
+        {tshirts.map((tshirt) => (
+          <TShirt
+            key={tshirt._id}
+            tshirt={tshirt}
+            handleAddToCart={handleAddToCart}
+          ></TShirt>
+        ))}
+      </div>
+
+      <div className="cart-container">
+        <Cart></Cart>
+      </div>
     </div>
   );
 };
