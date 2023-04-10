8014a26313fb69b96a9fd9214ad3fc722c6008eb
fattahsamit <fattahsamit@gmail.com>
2023-04-10 22:54:53 +0600

    tshirt react router

---
+++ Milestone_9/56/banglar-tshirt/src/components/About/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h3>this is the about section</h3>
+    </div>
+  );
+};
+
+export default About;
---
+++ Milestone_9/56/banglar-tshirt/src/components/OrderReview/OrderReview.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OrderReview = () => {
+  return (
+    <div>
+      <h3>this is orders</h3>
+    </div>
+  );
+};
+
+export default OrderReview;
