ff0b216aa601b7f0237ddf0038d9be89488a51b5
fattahsamit <fattahsamit@gmail.com>
2023-04-27 10:10:56 +0600

    load single data using GET api

---
+++ Milestone_10/phone-client/src/main.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./components/Main.jsx";
+import Phone from "./components/Phone.jsx";
+import Phones from "./components/Phones.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "phones",
+        element: <Phones />,
+        loader: () => fetch("http://localhost:3000/phones"),
+      },
+      {
+        path: "phone/:id",
+        element: <Phone />,
+        loader: ({ params }) =>
+          fetch(`http://localhost:3000/phones/${params.id}`),
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
--- Milestone_10/phone-server/index.js
+++ Milestone_10/phone-server/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const phones = require("./phones.json");
+var cors = require("cors");
 const app = express();
 const port = 3000;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Phone Server");
