bf5b70879e434ffc3ed9f2f49aa0fe0e7cbf4cb5
fattahsamit <fattahsamit@gmail.com>
2023-05-14 08:04:04 +0600

    React BS form

--- Milestone_10/email-pass-auth/src/main.jsx
+++ Milestone_10/email-pass-auth/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from "./components/Layout/Main.jsx";
@@ -8,6 +8,7 @@ import ErrorPage from "./components/ErrorPage/ErrorPage.jsx";
 import Home from "./components/Home/Home.jsx";
 import Register from "./components/Register/Register.jsx";
 import Login from "./components/Login/Login.jsx";
+import RegisterRBS from "./components/RegisterRBS/RegisterRBS.jsx";
 
 const router = createBrowserRouter([
   {
@@ -20,12 +21,16 @@ const router = createBrowserRouter([
         element: <Home></Home>,
       },
       {
-        path: "/login",
+        path: "login",
         element: <Login></Login>,
       },
       {
-        path: "/register",
+        path: "register",
         element: <Register></Register>,
+      },
+      {
+        path: "register-rbs",
+        element: <RegisterRBS></RegisterRBS>,
       },
     ],
   },
