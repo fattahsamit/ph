544ae21c0f26c1895faf99a45e3ad434eab9bc22
fattahsamit <fattahsamit@gmail.com>
2023-05-14 14:48:12 +0600

    form validation with success/error messages

--- Milestone_10/email-pass-auth/src/components/Register/Register.jsx
+++ Milestone_10/email-pass-auth/src/components/Register/Register.jsx
@@ -5,11 +5,14 @@ import app from "../firebase/firebase.config";
 const auth = getAuth(app);
 
 const Register = () => {
-  const [email, setEmail] = useState("");
+  // const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
 
   const handleSubmit = (event) => {
     // Prevent page refresh
     event.preventDefault();
+    setSuccess("");
     // Collect form data
     const email = event.target.email.value;
     const password = event.target.pass.value;
@@ -19,15 +22,19 @@ const Register = () => {
       .then((result) => {
         const loggedUser = result.user;
         console.log(loggedUser);
+        setError("");
+        event.target.reset();
+        setSuccess("User has been created successfully!");
       })
       .catch((error) => {
-        console.error(error);
+        console.error(error.message);
+        setError(error.message);
       });
   };
 
   const handleEmailChange = (event) => {
     // console.log(event.target.value);
-    setEmail(event.target.value);
+    // setEmail(event.target.value);
   };
 
   const handlePasswordBlur = (event) => {
@@ -45,6 +52,7 @@ const Register = () => {
           name="email"
           id="email"
           placeholder="Your Email Address"
+          required
         />
         <br />
         <input
@@ -54,8 +62,12 @@ const Register = () => {
           name="pass"
           id="pass"
           placeholder="Your Password"
+          required
         />
         <br />
+        <p className="text-danger">{error}</p>
+        <p className="text-success">{success}</p>
+
         <input className="btn btn-primary" type="submit" value="Register" />
       </form>
     </div>
