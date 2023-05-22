ada1670d0f7e9f6acdd3b89548638ed2a8dd9c6b
fattahsamit <fattahsamit@gmail.com>
2023-05-22 16:21:04 +0600

    reset password, show password

--- Milestone_10/email-pass-auth/src/components/Login/Login.jsx
+++ Milestone_10/email-pass-auth/src/components/Login/Login.jsx
@@ -1,5 +1,9 @@
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import React, { useState } from "react";
+import {
+  getAuth,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import React, { useRef, useState } from "react";
 import app from "../firebase/firebase.config";
 import { Link } from "react-router-dom";
 
@@ -8,6 +12,8 @@ const auth = getAuth(app);
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const emailRef = useRef();
+  const passRef = useRef();
 
   const [error, setError] = useState("");
   const [success, setSuccess] = useState("");
@@ -45,6 +51,35 @@ const Login = () => {
       });
   };
 
+  const showPassword = (event) => {
+    const pass = passRef.current.type;
+    console.log(pass);
+    if (pass === "password") {
+      passRef.current.type = "text";
+    } else {
+      passRef.current.type = "password";
+    }
+  };
+
+  const handleResetPassword = (event) => {
+    const email = emailRef.current.value;
+    if (!email) {
+      alert("Please provide your email address to reset password");
+      return;
+    }
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        // Password reset email sent!
+        alert("Please check your email");
+      })
+      .catch((error) => {
+        // const errorCode = error.code;
+        // const errorMessage = error.message;
+        console.log(error);
+        setError(error.message);
+      });
+  };
+
   return (
     <div className="w-25 mx-auto">
       <h2>Please Login</h2>
@@ -61,6 +96,7 @@ const Login = () => {
             placeholder="Enter your email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            ref={emailRef}
             required
           />
         </div>
@@ -76,13 +112,38 @@ const Login = () => {
             placeholder="Enter your password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            ref={passRef}
             required
           />
+          <button className="btn" onClick={showPassword}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+              <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+            </svg>
+          </button>
         </div>
         <button type="submit" className="btn btn-primary">
           Login
         </button>
       </form>
+      <p>
+        <small>
+          Forgot Password? Please
+          <button
+            onClick={handleResetPassword}
+            to="/register"
+            className="btn btn-link"
+          >
+            Reset Password
+          </button>
+        </small>
+      </p>
       <p>
         <small>
           New to this website? Please <Link to="/register">Register</Link>
