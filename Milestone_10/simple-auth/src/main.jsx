22c7e13b9b46ab023ef2807c48adbce4628f6484
fattahsamit <fattahsamit@gmail.com>
2023-05-23 16:47:35 +0600

    simple auth setup

---
+++ Milestone_10/simple-auth/src/main.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./components/Layout/MainLayout";
+import Home from "./components/Home/Home";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
+import Login from "./components/Login/Login";
+import Registration from "./components/Registration/Registration";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <Home></Home> },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Registration></Registration>,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
