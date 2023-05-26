c79047734f525997367edf4bf7543cf6addd61f3
fattahsamit <fattahsamit@gmail.com>
2023-05-26 18:48:59 +0600

    navbar with route set

--- Milestone_10/auth-firebase-context-tailwind/src/main.jsx
+++ Milestone_10/auth-firebase-context-tailwind/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "./layout/Main.jsx";
+import Login from "./components/Login";
+import Home from "./components/Home";
+import Register from "./components/Register";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
